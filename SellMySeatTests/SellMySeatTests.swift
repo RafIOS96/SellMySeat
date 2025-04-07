e1edd9d2614480913ca084f8ca7ad547a0a1df0d
Rafayel <rafael.aghayann@gmail.com>
2025-04-07 13:44:27 +0400

    Initial Commit

---
+++ SellMySeatTests/SellMySeatTests.swift
@@ -0,0 +1,17 @@
+//
+//  SellMySeatTests.swift
+//  SellMySeatTests
+//
+//  Created by Rafayel Aghayan on 07.04.25.
+//
+
+import Testing
+@testable import SellMySeat
+
+struct SellMySeatTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
